d3cc3f1626fca06473602212771abebef10a07d5
tomciv9000 <eugene.white@gmail.com>
2020-05-08 00:29:41 -0400

    Redux re-install

--- memory-lane-frontend/src/routes.js
+++ memory-lane-frontend/src/routes.js
@@ -1,5 +1,9 @@
-
 import React from 'react';
 import { Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
-import Homepage from './containers/Homepage';
+
+export default (
+    <Route path="/" >
+      
+    </Route>
+  );
