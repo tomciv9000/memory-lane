4c849d813a43c1d9ba24d8a7c4d5c146a89f25c0
tomciv9000 <eugene.white@gmail.com>
2020-05-07 23:50:07 -0400

    Build index.js starting point

--- memory-lane-frontend/src/index.js
+++ memory-lane-frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import Store from './Store';
+import routes from './routes'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
