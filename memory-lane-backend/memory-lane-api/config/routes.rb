eb9d738dec3d6fa9d3952390bc0fa00fce1e4b52
tomciv9000 <eugene.white@gmail.com>
2020-05-09 11:56:22 -0400

    jwt gem

--- memory-lane-backend/memory-lane-api/config/routes.rb
+++ memory-lane-backend/memory-lane-api/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :places
   resources :spots
   resources :memories
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
