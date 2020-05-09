f3619708c3d8cb3a403a18ae52e4d41dc6771263
tomciv9000 <eugene.white@gmail.com>
2020-05-08 21:26:25 -0400

    HomePage imports

--- memory-lane-frontend/src/App.js
+++ memory-lane-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './containers/HomePage';
 
 function App() {
   return (
---
+++ memory-lane-frontend/src/containers/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PlaceContainer from './PlaceContainer'
+import PlaceForm from './PlaceForm'
+// PropTypes can be used to typecheck a component or prop
+//import PropTypes from 'prop-types';
+import GoogleMaps from '../components/GoogleMaps'
+//import { List, Header, Grid, Image } from 'semantic-ui-react'
+import AllPlacesMap from '../components/AllPlacesMap'
+import { Link } from 'react-router-dom';
