cf2f021ccf22c3ba62d751ec849fa4e01fe89c3a
tomciv9000 <eugene.white@gmail.com>
2020-05-09 11:50:52 -0400

    User serializer fix

--- memory-lane-backend/memory-lane-api/app/serializers/place_serializer.rb
+++ memory-lane-backend/memory-lane-api/app/serializers/place_serializer.rb
@@ -1,5 +1,11 @@
 class PlaceSerializer
   include FastJsonapi::ObjectSerializer
-  
+  has_many :spots
+
+  def spots
+    object.spots.map { |e| {locationID: e.id, location: e.location, lat: e.lat, lng: e.lng, memories: e.memories.map{ |j| {memoryID: j.id, description: j.description, people: j.people, dates: j.dates, photos: j.photos, spotID: j.spot_id, userID: j.user_id} }} }
+  end
+
+
   attributes :name, :description, :user_id 
 end
