225f07049df15da770507a69ce51699e0f5cc2b5
tomciv9000 <eugene.white@gmail.com>
2020-05-10 22:05:42 -0400

    add login to routes

--- memory-lane-backend/memory-lane-api/config/routes.rb
+++ memory-lane-backend/memory-lane-api/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+ 
   resources :places
   resources :spots
   resources :memories
   resources :sessions
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
