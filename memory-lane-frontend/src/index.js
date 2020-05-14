6d22c505889911b51157210570d4ba943741adf0
tomciv9000 <eugene.white@gmail.com>
2020-05-14 12:18:30 -0400

    Add knock to bundle

--- memory-lane-frontend/src/index.js
+++ memory-lane-frontend/src/index.js
@@ -1,26 +1,32 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+//import { Provider } from 'react-redux';
+//import Store from './Store';
+//import routes from './routes';
+//import {Router, browserHistory} from 'react-router'
+//import {BrowserRouter, Route} from 'react-router-dom'
+//
+//
+//
+//ReactDOM.render(
+//  <Provider store={Store}>
+//    <React.StrictMode>
+//    <BrowserRouter>
+//      <App />
+//    </BrowserRouter>
+//    </React.StrictMode>
+//  </Provider>,
+//  document.getElementById('root')
+//);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import Store from './Store';
-import routes from './routes';
-import {Router, browserHistory} from 'react-router'
-import {BrowserRouter, Route} from 'react-router-dom'
 
-
-
-ReactDOM.render(
-  <Provider store={Store}>
-    <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
